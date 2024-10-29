cab118f42aed8548233f1cbf63c43ddec905d8ca
AndreewKah <134456211+AndreewKah@users.noreply.github.com>
2024-10-29 13:15:50 -0700

    Update Calculator_project.java

--- Calculator_project.java
+++ Calculator_project.java
@@ -1,5 +1,13 @@
+import java
 public class Calculator_project {
     public static void main(String[]args){
-        // blah blah blah
+        // pythagorean theorem, tan cos sin, circles
+
+        
+        // BMI and nutrition requirements
+    }
+    public static void BMIcalculator(int kg, int height){
+        double BMI = kg/(Math.pow(height,2));
+        
     }
 }
